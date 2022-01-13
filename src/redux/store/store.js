537e41186e0c12fa75ab9fc9f0873650948a2720
Ashutosh Bharti <ashutosh.bharti5247@gmail.com>
2022-01-13 17:02:29 +0530

    persist redux added, configured and working

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,15 @@
 import {createStore} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import RootReducer from '../reducers';
-const store = createStore(RootReducer);
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, RootReducer);
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
+export {store, persistor};
